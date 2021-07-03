bf1b44ca3315d20c92d283542fd70e564b83ebe4
Teng Wei Song <weisong.teng.work@gmail.com>
2021-07-04 01:55:36 +0800

    update login form

--- web-app/src/pages/Login.vue
+++ web-app/src/pages/Login.vue
@@ -2,20 +2,33 @@
   <page>
     <div class="columns">
       <div class="column">
-        <text-field
+        <field
           name="username"
-          title="User Name"
+          title="Username"
           :value="username"
+          type="text"
           icon="fa-user"
-          v-model="username"
-        ></text-field>
-        <password-field
+          v-model="$v.username.$model"
+          ><template v-slot:errorMessages>
+            <p class="help is-danger" v-if="!$v.username.required">
+              Username is required
+            </p>
+          </template></field
+        >
+        <field
           name="password"
           title="Password"
           :value="password"
+          type="password"
           icon="fa-key"
-          v-model="password"
-        ></password-field>
+          v-model="$v.password.$model"
+        >
+          <template v-slot:errorMessages>
+            <p class="help is-danger" v-if="!$v.password.required">
+              Password is required
+            </p>
+          </template></field
+        >
         <div class="field is-grouped">
           <div class="control">
             <button class="button is-primary" @click="login">Log In</button>
@@ -29,41 +42,52 @@
 
 <script>
 import Page from "../components/Page.vue";
-import TextField from "../components/controls/TextField.vue";
-import PasswordField from "../components/controls/PasswordField.vue";
+import Field from "../components/Field.vue";
 import authService from "../services/authService";
+import { required } from "vuelidate/lib/validators";
 
 export default {
-  components: { Page, TextField, PasswordField },
+  components: { Page, Field },
   data() {
     return {
       username: "",
       password: ""
     };
   },
+  validations: {
+    username: {
+      required
+    },
+    password: {
+      required
+    }
+  },
   methods: {
     login() {
-      authService
-        .login(this.username, this.password)
-        .then(resp => {
-          this.$store.dispatch("auth/login", {
-            username: this.username,
-            data: resp.data
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        authService
+          .login(this.username, this.password)
+          .then(resp => {
+            this.$store.dispatch("auth/login", {
+              username: this.username,
+              data: resp.data
+            });
+            this.$store.dispatch("alert/success", {
+              heading: "Logged in"
+            });
+            this.$router.push(this.$route.query.redirectTo || "/");
+          })
+          .catch(resp => {
+            console.log(resp);
+            localStorage.removeItem("accessToken");
+            this.$store.dispatch("auth/logout");
+            this.$store.dispatch("alert/danger", {
+              heading: "Unable to log in",
+              message: "Wrong username or password."
+            });
           });
-          this.$store.dispatch("alert/success", {
-            heading: "Logged in"
-          });
-          this.$router.push(this.$route.query.redirectTo || "/");
-        })
-        .catch(resp => {
-          console.log(resp);
-          localStorage.removeItem("accessToken");
-          this.$store.dispatch("auth/logout");
-          this.$store.dispatch("alert/danger", {
-            heading: "Unable to log in",
-            message: "Wrong username or password."
-          });
-        });
+      }
     }
   }
 };
