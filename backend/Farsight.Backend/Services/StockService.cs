aad79e41da11b229d38067bba9df4af246ab64e4
Teng Wei Song <weisong.teng.work@gmail.com>
2021-08-04 11:39:54 +0800

    cache and catch exception for stock service

--- backend/Farsight.Backend/Services/StockService.cs
+++ backend/Farsight.Backend/Services/StockService.cs
@@ -7,6 +7,7 @@ using Farsight.Backend.Extensions;
 using Farsight.Backend.Models.DTOs;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Farsight.Backend.Services
 {
@@ -15,95 +16,136 @@ namespace Farsight.Backend.Services
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _configuration;
         private readonly IMemoryCache _memoryCache;
+        private readonly ILogger _logger;
 
-        public StockService(IHttpClientFactory httpClientFactory, IConfiguration configuration, IMemoryCache memoryCache)
+        public StockService(IHttpClientFactory httpClientFactory, IConfiguration configuration, IMemoryCache memoryCache, ILogger logger)
         {
             _httpClientFactory = httpClientFactory;
             _configuration = configuration;
             _memoryCache = memoryCache;
+            _logger = logger;
         }
 
         public async Task<IList<PolygonTicker>> GetTickers()
         {
-            List<PolygonTicker> polygonTickers;
+            List<PolygonTicker> polygonTickers = null;
 
             if (_memoryCache.TryGetValue<List<PolygonTicker>>("tickers.all", out polygonTickers))
                 return polygonTickers;
 
-            var client = _httpClientFactory.CreateClient("polygon");
-            var apiKey = _configuration["polygon:ApiKey"];
-
-            var response = await client.GetAsync($"/v3/reference/tickers?limit=1000&market=stocks&apiKey={apiKey}");
-            response.EnsureSuccessStatusCode();
-
-            var polygonTickersResponse = JsonSerializer.Deserialize<PolygonTickersResponse>(await response.Content.ReadAsStringAsync());
-            polygonTickers = polygonTickersResponse.Results.ToList();
-
-            while (!string.IsNullOrWhiteSpace(polygonTickersResponse.NextUrl))
+            try
             {
-                response = await client.GetAsync($"{polygonTickersResponse.NextUrl}&apiKey={apiKey}");
+                var client = _httpClientFactory.CreateClient("polygon");
+                var apiKey = _configuration["polygon:ApiKey"];
+
+                var response = await client.GetAsync($"/v3/reference/tickers?limit=1000&market=stocks&apiKey={apiKey}");
                 response.EnsureSuccessStatusCode();
-                polygonTickersResponse = JsonSerializer.Deserialize<PolygonTickersResponse>(await response.Content.ReadAsStringAsync());
-                polygonTickers.AddRange(polygonTickersResponse.Results);
+
+                var polygonTickersResponse = JsonSerializer.Deserialize<PolygonTickersResponse>(await response.Content.ReadAsStringAsync());
+                polygonTickers = polygonTickersResponse.Results.ToList();
+
+                while (!string.IsNullOrWhiteSpace(polygonTickersResponse.NextUrl))
+                {
+                    response = await client.GetAsync($"{polygonTickersResponse.NextUrl}&apiKey={apiKey}");
+                    response.EnsureSuccessStatusCode();
+                    polygonTickersResponse = JsonSerializer.Deserialize<PolygonTickersResponse>(await response.Content.ReadAsStringAsync());
+                    polygonTickers.AddRange(polygonTickersResponse.Results);
+                }
+
+                polygonTickers = polygonTickers.DistinctBy(pt => pt.Ticker).ToList();
+
+                _memoryCache.Set<List<PolygonTicker>>("tickers.all", polygonTickers);
             }
-
-            polygonTickers = polygonTickers.DistinctBy(pt => pt.Ticker).ToList();
-
-            _memoryCache.Set<List<PolygonTicker>>("tickers.all", polygonTickers);
+            catch (HttpRequestException e)
+            {
+                _logger.Error(e.ToString());
+            }
 
             return polygonTickers;
         }
 
         public async Task<PolygonTickerDetails> GetTickerDetails(string ticker)
         {
-            PolygonTickerDetails polygonTickerDetails;
+            PolygonTickerDetails polygonTickerDetails = null;
 
             if (_memoryCache.TryGetValue<PolygonTickerDetails>($"tickers.details.{ticker}", out polygonTickerDetails))
                 return polygonTickerDetails;
 
-            var client = _httpClientFactory.CreateClient("polygon");
-            var apiKey = _configuration["polygon:ApiKey"];
+            try
+            {
+                var client = _httpClientFactory.CreateClient("polygon");
+                var apiKey = _configuration["polygon:ApiKey"];
 
-            var response = await client.GetAsync($"/v1/meta/symbols/{ticker}/company?apiKey={apiKey}");
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync();
-            polygonTickerDetails = JsonSerializer.Deserialize<PolygonTickerDetails>(content);
+                var response = await client.GetAsync($"/v1/meta/symbols/{ticker}/company?apiKey={apiKey}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                polygonTickerDetails = JsonSerializer.Deserialize<PolygonTickerDetails>(content);
 
-            _memoryCache.Set<PolygonTickerDetails>($"tickers.details.{ticker}", polygonTickerDetails);
+                _memoryCache.Set<PolygonTickerDetails>($"tickers.details.{ticker}", polygonTickerDetails);
+
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.Error(e.ToString());
+            }
 
             return polygonTickerDetails;
         }
 
         public async Task<PolygonAggregatesResponse> GetDailyClosePrice(string ticker, string from, string to)
         {
-            var client = _httpClientFactory.CreateClient("polygon");
-            var apiKey = _configuration["polygon:ApiKey"];
+            PolygonAggregatesResponse dailyPriceResponse = null;
 
-            var response = await client.GetAsync($"/v2/aggs/ticker/{ticker}/range/1/day/{from}/{to}?adjusted=true&sort=asc&apiKey={apiKey}");
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync();
+            if (_memoryCache.TryGetValue<PolygonAggregatesResponse>($"tickers.dailyClose.{ticker}.{from}.{to}", out dailyPriceResponse))
+                return dailyPriceResponse;
 
-            return JsonSerializer.Deserialize<PolygonAggregatesResponse>(content);
+            try
+            {
+                var client = _httpClientFactory.CreateClient("polygon");
+                var apiKey = _configuration["polygon:ApiKey"];
+
+                var response = await client.GetAsync($"/v2/aggs/ticker/{ticker}/range/1/day/{from}/{to}?adjusted=true&sort=asc&apiKey={apiKey}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                dailyPriceResponse = JsonSerializer.Deserialize<PolygonAggregatesResponse>(content);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(System.TimeSpan.FromHours(12));
+                _memoryCache.Set<PolygonAggregatesResponse>($"tickers.dailyClose.{ticker}.{from}.{to}", dailyPriceResponse, cacheEntryOptions);
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.Error(e.ToString());
+            }
+
+            return dailyPriceResponse;
         }
 
         public async Task<PolygonAggregatesResponse> GetPreviousClosePrice(string ticker)
         {
-            PolygonAggregatesResponse previousClosePriceResponse;
+            PolygonAggregatesResponse previousClosePriceResponse = null;
 
             if (_memoryCache.TryGetValue<PolygonAggregatesResponse>($"tickers.previousClose.{ticker}", out previousClosePriceResponse))
                 return previousClosePriceResponse;
 
-            var client = _httpClientFactory.CreateClient("polygon");
-            var apiKey = _configuration["polygon:ApiKey"];
+            try
+            {
+                var client = _httpClientFactory.CreateClient("polygon");
+                var apiKey = _configuration["polygon:ApiKey"];
 
-            var response = await client.GetAsync($"/v2/aggs/ticker/{ticker}/prev?adjusted=true&apiKey={apiKey}");
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync();
-            previousClosePriceResponse = JsonSerializer.Deserialize<PolygonAggregatesResponse>(content);
+                var response = await client.GetAsync($"/v2/aggs/ticker/{ticker}/prev?adjusted=true&apiKey={apiKey}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                previousClosePriceResponse = JsonSerializer.Deserialize<PolygonAggregatesResponse>(content);
 
-            var cacheEntryOptions = new MemoryCacheEntryOptions()
-                .SetAbsoluteExpiration(System.TimeSpan.FromHours(1));
-            _memoryCache.Set<PolygonAggregatesResponse>($"tickers.previousClose.{ticker}", previousClosePriceResponse, cacheEntryOptions);
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(System.TimeSpan.FromHours(1));
+                _memoryCache.Set<PolygonAggregatesResponse>($"tickers.previousClose.{ticker}", previousClosePriceResponse, cacheEntryOptions);
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.Error(e.ToString());
+            }
 
             return previousClosePriceResponse;
         }
